f32fd6974c4f322271c2c40b66ecdf440f4f7038
beatrice120 <beatrice120>
2020-11-16 11:50:06 +0200

    adding tesfile

--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -4,6 +4,34 @@ import static org.junit.Assert.*;
 
 public class CaesarCipherTest {
     @Test
-    public void name() {
+    public void uppercase() {
+        char d='G';
+        char expectedc = 'G';
+        assertEquals(expectedc, G);
+    }
+
+    @Test
+    public void myword() {
+
+        public void chararray () {
+            char ch = 'a';
+            char expectedch = 'a';
+            assertEquals(expectedch, ch);
+        }
+        @Test
+        public void uppercase () {
+            char c = 'B';
+            char expectedc = 'B';
+            assertEquals(expectedc, c);
+        }
+
+
+
+        @Test
+        public void lowercase () {
+            char c = 'd';
+            char expectedc = 'd';
+            assertEquals(expectedc, c);
+        }
     }
 }
